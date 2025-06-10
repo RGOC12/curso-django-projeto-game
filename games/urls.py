d7808016b8fac4e962e3b8e589e2e117a185f4e8
Ramon Gall <oliveiraagall@gmail.com>
2025-06-10 15:00:26 -0300

    Primeiro commit

---
+++ games/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from games.views import home
+urlpatterns = [
+    path('', home)
+]
---
+++ games/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+
+
+def home(request):
+    return render(request,'games/pages/home.html')
