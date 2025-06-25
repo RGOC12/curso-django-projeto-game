5abf939445c77e453e84c9af7b5321730dde513f
oliveiraagall <oliveiraagall@gmail.com>
2025-06-25 03:25:27 +0000

    Implementação do conteudo fake.

--- games/views.py
+++ games/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from utils.games.games import make_games
 
 
 def home(request):
-    return render(request,'games/pages/home.html')
+    return render(request,'games/pages/home.html', context={
+        'games': [make_games() for _ in range(10)]
+    })
 
 def game(request,id):
-    return render(request,'games/pages/game.html')
+    return render(request,'games/pages/game.html', context={
+        'game': make_games()
+    })
